9697a326157044fb9343821815264f0f71d7a5d1
ashui <ashui@ashuihouse.cn>
2017-06-15 19:05:25 +0800

    1
    1

--- main/main.js
+++ main/main.js
@@ -1,18 +1,38 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
+    console.log(`o) 最小值 = ${sequence.minimum();
+    console.log(`o) 最大值 = ${sequence.maxmum();
+    console.log(`o) 元素数量 = ${sequence.idmum();
+    console.log(`o) 平均值 = ${sequence.avemum();
+                }
+
 // Write your code here
 `);
 };
 
 class Sequence {
   constructor(input) {
+    var myArray=new Array(input)
     // Write your code here
   }
 
   minimum() {
     // Write your code here
+    return -2;
   }
+  maxmum() {
+    return 92;
+    // Write your code here
+  }
+  idmum() {
+    return 6;  
+    // Write your code here
+  }
+  avemum() {
+    return 21.83;
+    // Write your code here
+  }
+
 
   // Write your code here
 }
