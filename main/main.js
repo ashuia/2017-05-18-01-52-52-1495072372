b0e3d6aa8403aff3fd1e402b4e38298a2049acf9
ashui <ashui@ashuihouse.cn>
2017-06-22 23:20:04 +0800

    1
    1

--- main/main.js
+++ main/main.js
@@ -1,33 +1,39 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}\no) 最大值 = ${sequence.maxmum()}\no) 元素数量 = ${sequence.idmum()}\no) 平均值 = ${sequence.avemum()}`);
-
-             // Write your code here
+    console.log('o) 最小值 = '+sequence.minimum()+
+'\no) 最大值 = '+sequence.maxmu()+
+'\no) 元素数量 = '+sequence.len+
+'\no) 平均值 = '+sequence.average());
 };
 
 class Sequence {
   constructor(input) {
-    var myArray=new Array(input);
-    // Write your code here
+      this.len = input.length;
+      this.input = input;
   }
 
   minimum() {
-    // Write your code here
-    return -2;
-  }
-  maxmum() {
-    return 92;
-    // Write your code here
-  }
-  idmum() {
-    return 6;  
-    // Write your code here
-  }
-  avemum() {
-    return 21.83;
-    // Write your code here
+    var min=this.input[0];
+    for(var i=1;i<this.len;i++) {
+        if(this.input[i]<min)
+        min=this.input[i];
+    }
+    return min;
   }
 
-
-  // Write your code here
+  maxmu(){
+    var max=this.input[0];
+    for(var i=1;i<this.len;i++) {
+        if(this.input[i]>max)
+        max=this.input[i];
+    }
+    return max;
+  }
+  average(){
+	  var av=0;
+	  for(var i=0;i<this.len;i++) {
+        av=av+this.input[i];
+    }
+    return (av/this.len).toFixed(2);
+  }
 }
